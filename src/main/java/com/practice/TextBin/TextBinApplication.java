f13362c357d73b1ef4466eee927623a1e5f6360b
Tofayel Ahmed <tajuddinahmed450@gmail.com>
2022-05-05 17:49:30 +0600

    project created

---
+++ src/main/java/com/practice/TextBin/TextBinApplication.java
@@ -0,0 +1,13 @@
+package com.practice.TextBin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/TextBin/TextBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.TextBin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
